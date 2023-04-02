ca1fba4fd52098879fd8dee171d4fd122a7457e1
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-04-02 16:02:37 +0100

    adding view character5

--- app/src/main/java/com/simon/harrypotter/ui/screens/viewCharacter/ViewCharacter.kt
+++ app/src/main/java/com/simon/harrypotter/ui/screens/viewCharacter/ViewCharacter.kt
@@ -1,19 +1,14 @@
 package com.simon.harrypotter.ui.screens.viewCharacter
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -26,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.simon.data.data.network.repositories.NetworkResult
 import com.simon.data.models.characters.isMale
 import com.simon.harrypotter.R
@@ -34,7 +30,6 @@ import com.simon.harrypotter.core.viewModels.Events
 import com.simon.harrypotter.ui.components.BodyText
 import com.simon.harrypotter.ui.components.TitleTextMedium
 import com.simon.harrypotter.ui.components.appbars.SimonAppBars
-import com.simon.harrypotter.ui.components.buttons.SimonIconButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -49,11 +44,23 @@ fun ViewCharacter(characterId:String,appViewModel:AppViewModel ) {
         }
     }
 
+    val systemController = rememberSystemUiController()
+    val darkTheme = isSystemInDarkTheme()
+    DisposableEffect( true ){
+        systemController.setStatusBarColor(Color.Transparent,false)
+        onDispose {
+            systemController.setStatusBarColor(Color.Transparent,!darkTheme)
+        }
+    }
+
     Box() {
         
         AsyncImage(model =R.drawable.cover , contentDescription = null,
-        modifier = Modifier.fillMaxWidth().fillMaxHeight(0.4f)
-            .align(Alignment.TopCenter).alpha(0.8f), contentScale = ContentScale.Crop)
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.4f)
+            .align(Alignment.TopCenter)
+            .alpha(0.8f), contentScale = ContentScale.Crop)
         
         Scaffold(
             topBar = {
